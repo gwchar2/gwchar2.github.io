44f4807fcf1f501bac1ea4bbfe51e4c44660189a
Tommer <137009790+gwchar2@users.noreply.github.com>
2025-03-29 00:40:12 +0300

    Attempt

---
+++ js/script.js
@@ -0,0 +1,25 @@
+VANTA.NET({
+    el: "#vanta-left",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 300.00,
+    minWidth: 300.00,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0x000000,
+    backgroundColor: 0xffffff
+})
+
+VANTA.NET({
+    el: "#vanta-right",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0xffffff,          // white lines
+    backgroundColor: 0x000000 // black background
+  });
