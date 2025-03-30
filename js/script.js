cd3ddfa9bb22835dd89866c179c3cc8fec35cdd3
Tommer <137009790+gwchar2@users.noreply.github.com>
2025-03-30 03:49:22 +0300

    Landing page complete

--- js/script.js
+++ js/script.js
@@ -19,6 +19,20 @@ if (window.innerWidth > 700) {
       scale: 1.0,
       scaleMobile: 1.0
     });
+    
+    const menuToggle = document.getElementById("menu-toggle");
+    const navWrapper = document.querySelector(".nav-wrapper");
+    const sidebarLinks = document.querySelectorAll("#sidebar a");
+
+    menuToggle.addEventListener("click", () => {
+      navWrapper.classList.toggle("open");
+    });
+
+    sidebarLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        navWrapper.classList.remove("open");
+      });
+    });
   } else {
     // Mobile/small: Vanta only on left side, both sides white
     VANTA.NET({
